49286c1e88ef0a6f4e951b4c561c23c8d5ac1b23
Preil <preil2000@gmail.com>
2020-08-30 13:36:13 +0300

    Firebase config updated. Saving new project to db.

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,14 +10,14 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
       console.log('created project ', action.project);
-
       return {
         projects: [...state.projects, action.project]
-      }
-  }
-
-  return {
-    ...state
+      };
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.error)
+      return state;
+    default:
+      return state
   }
 };
 
